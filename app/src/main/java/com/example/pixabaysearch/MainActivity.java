9a5db961a468d2c6833868e0baf5ef30f9e5ceb3
MillyLee <bubble0714.emily@gmail.com>
2016-10-02 15:17:34 +0800

    Add searchable function

--- app/src/main/java/com/example/pixabaysearch/MainActivity.java
+++ app/src/main/java/com/example/pixabaysearch/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.pixabaysearch;
 
+import android.os.Bundle;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.ComponentName;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,48 +21,59 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int LIST_SPAN_COUNT = 1;
     private static final int GRID_SPAN_COUNT = 3;
-    private static final int STATE_LIST = 1;
-    private static final int STATE_GRID = 2;
-    private int mCurrentState = STATE_LIST;
+    private static final int STATE_GRID = 1;
+    private static final int STATE_LIST = 2;
+    private int spanState = STATE_GRID;
     private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager recyclerLayoutManager;
-    private StaggeredGridLayoutManager gaggeredGridLayoutManager;
+    private StaggeredGridLayoutManager recyclerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar my_toolbar = (Toolbar) findViewById(R.id.search_toolbar);
-        setSupportActionBar(my_toolbar);
+        initViews();
+        setGaggeredListView();
+    }
 
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerLayoutManager = new StaggeredGridLayoutManager(LIST_SPAN_COUNT,
-                StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(recyclerLayoutManager);
+    public void initViews() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.search_toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent searchIntent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(searchIntent.getAction())) {
+            String keyword = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(MainActivity.this, keyword, Toast.LENGTH_SHORT).show();
+        }
+
+        //Init state is grid view
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(false);
-        setGaggeredGridView();
     }
 
     public void setGaggeredGridView(){
-
-        gaggeredGridLayoutManager = new StaggeredGridLayoutManager(GRID_SPAN_COUNT, 1);
-        recyclerView.setLayoutManager(gaggeredGridLayoutManager);
-
-        List<ItemObject> gaggeredList = getListItemData();
-
-        RecyclerAdapter rcAdapter = new RecyclerAdapter(MainActivity.this, gaggeredList, true);
-        recyclerView.setAdapter(rcAdapter);
+        spanState = STATE_GRID;
+        recyclerLayoutManager = new StaggeredGridLayoutManager(GRID_SPAN_COUNT,
+                StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        recyclerView.setAdapter(new RecyclerAdapter(MainActivity.this, getListItemData()));
     }
 
-    private List<ItemObject> getListItemData(){
-        List<ItemObject> listViewItems = new ArrayList<ItemObject>();
+    public void setGaggeredListView() {
+        spanState = STATE_LIST;
+        recyclerLayoutManager = new StaggeredGridLayoutManager(LIST_SPAN_COUNT, 1);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        recyclerView.setAdapter(new RecyclerAdapter(MainActivity.this, getListItemData()));
+    }
+
+    private List<ItemContent> getListItemData() {
+        List<ItemContent> listViewItems = new ArrayList<ItemContent>();
         Drawable myIcon = getResources().getDrawable(R.drawable.sample_0);
         Drawable myIcon1 = getResources().getDrawable(R.drawable.sample_1);
-        listViewItems.add(new ItemObject(myIcon, "1","2"));
-        listViewItems.add(new ItemObject(myIcon1, "1","2"));
-        listViewItems.add(new ItemObject(myIcon, "1","2"));
-        listViewItems.add(new ItemObject(myIcon1, "1","2"));
+        listViewItems.add(new ItemContent(myIcon, "1", "2"));
+        listViewItems.add(new ItemContent(myIcon1, "1", "2"));
+        listViewItems.add(new ItemContent(myIcon, "1", "2"));
+        listViewItems.add(new ItemContent(myIcon1, "1", "2"));
 
         return listViewItems;
     }
@@ -66,6 +82,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -76,19 +96,22 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case R.id.menu_gridview:
+                spanState = STATE_GRID;
                 recyclerLayoutManager = new StaggeredGridLayoutManager(GRID_SPAN_COUNT,
                         StaggeredGridLayoutManager.VERTICAL);
                 recyclerView.setLayoutManager(recyclerLayoutManager);
                 break;
 
             case R.id.menu_listview:
+                spanState = STATE_LIST;
                 recyclerLayoutManager = new StaggeredGridLayoutManager(LIST_SPAN_COUNT,
                         StaggeredGridLayoutManager.VERTICAL);
                 recyclerView.setLayoutManager(recyclerLayoutManager);
                 break;
 
         }
-        invalidateOptionsMenu();
         return super.onOptionsItemSelected(item);
     }
 }
+
+
--- app/src/main/java/com/example/pixabaysearch/RecyclerAdapter.java
+++ app/src/main/java/com/example/pixabaysearch/RecyclerAdapter.java
@@ -1,10 +1,13 @@
 package com.example.pixabaysearch;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,40 +18,64 @@ import java.util.List;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerViewHolder>{
 
     private Context mContext;
-    private List<ItemObject> mImageList;
-    private boolean mIsGrid = true;
+    private List<ItemContent> mImageList;
 
-    public RecyclerAdapter(Context context, List<ItemObject> imageList, boolean isGrid) {
+    public RecyclerAdapter(Context context, List<ItemContent> imageList) {
         mContext = context;
         mImageList = imageList;
-        mIsGrid = isGrid;
     }
 
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, null);
-        if (!mIsGrid)
-            layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_row, null);
         RecyclerViewHolder imageViewHolder = new RecyclerViewHolder(layoutView);
         return imageViewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerViewHolder holder, final int position) {
-        holder.mImageView.setImageDrawable(mImageList.get(position).mImageDrawable);
-        holder.mTextView.setText(mImageList.get(position).mImageDescription);
-        holder.mCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        holder.mImageView.setImageDrawable(mImageList.get(position).imgSource);
+        holder.mTextView.setText(mImageList.get(position).imgTitle);
+//        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
 //                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mImageList.get(position).mImageUrl));
 //                mContext.startActivity(browserIntent);
-            }
-        });
+//            }
+//        });
     }
 
     @Override
     public int getItemCount() {
         return mImageList.size();
+    }
+}
+
+class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView mImageView;
+    public TextView mTextView;
+//    public CardView mCardView;
+
+    public RecyclerViewHolder(View itemView) {
+        super(itemView);
+
+        mImageView = (ImageView) itemView.findViewById(R.id.image_res);
+        mTextView = (TextView) itemView.findViewById(R.id.image_description);
+//        mCardView = (CardView) itemView.findViewById(R.id.image_card);
+    }
+}
+
+class ItemContent {
+
+    public Drawable imgSource;
+    public String imgTitle;
+    public String imgUrl;
+
+    public ItemContent(Drawable image, String title, String url) {
+        imgSource = image;
+        imgTitle = title;
+        imgUrl = url;
     }
 }
 
